7597e93f4f4e5d3148ab511e3d53204112039fe5
cudilara <dilara.madinger@colorado.edu>
2018-03-08 06:09:44 -0700

    general structure

---
+++ project1/milestone3/Linux4Ltls/Linux4Ltls/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Linux4Ltls
+//
+//  Created by Dilara Madinger on 3/7/18.
+//  Copyright © 2018 Dilara Madinger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
